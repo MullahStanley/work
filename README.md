d0a9de1cd8d2c6f1d5269ec9056c18d7368b4886
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-04-03 07:19:43 +0300

    Assignment review

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# work
+# Code Challenge
+When the site is loaded, the first movie is displayed along with its poster,title, runtime and showtime.
+On the left section is the movies list showing all the movies in an ordered-list.
+The site allows you to buy tickets by clicking the Get tickets button.
+If the number of tickets available is below one, an alert is shown displaying that no tickets are available.
--- index.js
+++ index.js
@@ -1,22 +1,70 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('http://localhost:3000/films/1')
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(films => {
-        const details= document.querySelector("#main-1")
-        const tickets = `${tickets_sold}` - `${capacity}`;
-        const list=document.createElement("li")
-        list.innerHTML=`
-        <img src="${poster}" class="card-img-top" alt="poster">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text">${runtime}.</p>
-          <p class="card-text">${showtime}</p>
-          <p class="card-text">${tickets}</p>
-          <a href="#" class="btn btn-primary">Buy Ticket</a>
-        </div>
-      `
-  details.appendChild(list)
-    })
-  })
-})
+//tickets expression
+const tickets= `${film.capacity}` -`${film.tickets_sold}`;
+
+//first function
+function showFilm(film) {
+  let card = document.createElement("div");
+  document.addEventListener("load", () => {
+    const films= document.querySelector("#films")
+    card.innerHTML = `
+     
+      <img id="image" src=${film.poster} alt="${film.title}" />
+      <h3>${film.title}</h3>
+      <h3>${film.runtime}</h3>
+      <h3>${film.showtime}</h3>
+      <h3> Available Tickets:${tickets}</h3>
+    `;
+  //Append the data to the DOM.
+    films.appendChild(card);
+  });
+}
+///Fetch the first movie
+function fetchFilm() {
+  fetch("http://localhost:3000/film/1")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .then((data) => showFilm(data));
+  loadFirstFilm();
+  allFilms();
+}
+
+//load the first movie
+function loadFirstFilm() {
+  fetchFilm();
+}
+
+
+// fetches all movies
+function allFilms() {
+  fetch("http://localhost:3000/film")
+    .then((res) => res.json())
+    .then((data) => data.forEach((film) => filmMenu(film)));
+}
+
+//creates the ordered lists to append to the left section
+function filmMenu(film) {
+  const list = document.createElement("ol");
+  list.innerHTML = `
+      <li>
+      <img id="image"src=${film.poster} alt="" />
+      <h3>${film.title}</h3>
+      <h3>${film.runtime}</h3>
+      <h3>${film.capacity}</h3>
+      <h3>${film.showtime}</h3>
+      <h3>${film.description}</h3>
+      <h3>${tickets}</h3>
+      </li> 
+        `
+  document.querySelector("#left-section").append(list);
+}
+
+///button event
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  let availableTickets = tickets;
+  if (availableTickets >= 1) {
+    availableTickets-1;
+  } else if(availableTickets<1){
+    alert("Tickets are no longer available");
+  }
+});
