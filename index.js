5bcff0d4a431194f936823d7e5b8cf0301d80a8a
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-04-03 06:47:40 +0300

    Second Commit

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-const basesrc= "http://localhost:3000/films";
-const details=document.querySelector("#section-1")
-function showFilm(films){
-    
-}
 //first step
-function displayFilm(films){
+/*document.addEventListener("DOMContentLoaded",function displayFilm(films){
     const tickets = `${tickets_sold}` - `${capacity}`;
     const list=document.createElement("li")
     list.innerHTML=`
-  <img src="`${poster}`" class="card-img-top" alt="poster">
+  <img src="${poster}" class="card-img-top" alt="poster">
   <div class="card-body">
-    <h5 class="card-title">`${title}`</h5>
-    <p class="card-text">`${runtime}`.</p>
-    <p class="card-text">`${showtime}`</p>
-    <p class="card-text">`${tickets}`</p>
+    <h5 class="card-title">${title}</h5>
+    <p class="card-text">${runtime}.</p>
+    <p class="card-text">${showtime}</p>
+    <p class="card-text">${tickets}</p>
     <a href="#" class="btn btn-primary">Buy Ticket</a>
   </div>
     `
   details.appendChild(list)
-}
+})
 
 
 
 //fetch requests
 function showFilm(){
-    fetch("http://localhost:3000/films")
+    fetch("http://localhost:3000/films/1")
     .then(res=> res.json())
-    .then()
-}
+    .then(films => films.forEach(element => {
+      
+    })
+    )
+}*/
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:3000/films/1')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(films => {
+        const details= document.querySelector("#main-1")
+        const tickets = `${tickets_sold}` - `${capacity}`;
+        const list=document.createElement("li")
+        list.innerHTML=`
+        <img src="${poster}" class="card-img-top" alt="poster">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${runtime}.</p>
+          <p class="card-text">${showtime}</p>
+          <p class="card-text">${tickets}</p>
+          <a href="#" class="btn btn-primary">Buy Ticket</a>
+        </div>
+      `
+  details.appendChild(list)
+    })
+  })
+})
